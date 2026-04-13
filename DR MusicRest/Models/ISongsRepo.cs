ac268449bb05655e838ff804d8d1486a34572464
Ahmed Hemdan <aha007@edu.zealand.dk>
2026-04-13 10:25:09 +0200

    1

---
+++ DR MusicRest/Models/ISongsRepo.cs
@@ -0,0 +1,11 @@
+﻿namespace DR_MusicRest.Models
+{
+    public interface ISongsRepo
+    {
+        List<Song> GetAll();
+        Song Add(Song song);
+        Song? GetSongById(int id);
+        Song? RemoveSong(int id);
+        Song? UpdateSong(int id, Song updatedSong);
+    }
+}
