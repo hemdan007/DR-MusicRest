8357063eb06318a53e7eecaccf5b24df5608c6f4
Ahmed Hemdan <aha007@edu.zealand.dk>
2026-04-13 11:36:08 +0200

    1finish

---
+++ DR MusicRest/Controllers/SongsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DR_MusicRest.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DR_MusicRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private readonly ISongsRepo _songsRepo;
+
+        public SongsController(ISongsRepo songsRepo)
+        {
+            _songsRepo = songsRepo;
+        }
+
+        // GET: api/<SongsController>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<List<Song>> Get()
+        {
+            return Ok(_songsRepo.GetAll());
+        }
+
+        // GET api/<SongsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SongsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SongsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SongsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- DR MusicRest/Program.cs
+++ DR MusicRest/Program.cs
@@ -1,9 +1,35 @@
+using DR_MusicRest.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure CORS to allow requests from any origin, method, and header
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll",
+                              policy =>
+                              {
+                                  policy
+                                  .AllowAnyOrigin()
+                                  .AllowAnyMethod()
+                                  .AllowAnyHeader();
+                              });
+});
+
+
+
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+
+
+// Dependency Injection: Register the SongsRepoList as the implementation for ISongsRepo
+builder.Services.AddSingleton<ISongsRepo, SongsRepoList>();
+
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -13,6 +39,9 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+// Enable CORS using the defined policy
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
