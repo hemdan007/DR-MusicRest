dd482ea9ff806363b66c77e675097f9c813e25b4
Ahmed Hemdan <aha007@edu.zealand.dk>
2026-04-17 11:12:39 +0200

    Swagger and publish to Azure

--- DR MusicRest/Program.cs
+++ DR MusicRest/Program.cs
@@ -28,6 +28,9 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
+// Swagger/OpenAPI configuration
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
 // Dependency Injection: Register the SongsRepoList as the implementation for ISongsRepo
@@ -73,6 +76,10 @@ builder.Services.AddAuthorization();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+//swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
